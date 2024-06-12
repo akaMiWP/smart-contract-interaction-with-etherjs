0446cac397a53afcbeeae69958dc0f8ea1083486
akaMiWP <bangkokappdevelopment@gmail.com>
2024-06-12 21:40:34 +0700

    Modifed a contract

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,10 +3,19 @@
 pragma solidity ^0.8.0;
 
 contract HelloWorld {
-    string public name;
+    string private message;
     address private owner;
 
     constructor() {
         owner = msg.sender;
+    }
+
+    function changeMessage(string memory _message) external {
+        require(msg.sender == owner, "Sorry, this is only for the owner");
+        message = _message;
+    }
+
+    function getMessage() external view returns (string memory) {
+        return message;
     }
 }
